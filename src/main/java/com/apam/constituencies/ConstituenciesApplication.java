d6c84a15f1e37c4833355415107efcad3f6b2e01
iapam <apamibrahim714@gmail.com>
2023-11-07 17:34:55 +0000

    the app

---
+++ src/main/java/com/apam/constituencies/ConstituenciesApplication.java
@@ -0,0 +1,13 @@
+package com.apam.constituencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstituenciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConstituenciesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apam/constituencies/ConstituenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apam.constituencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstituenciesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
